8f893de44e0281e302ccf579ea74a389b0ef3b2d
bread sticks <uubixx.0208@gmail.com>
2024-08-22 17:42:44 +0000

    finished task I-N

--- wordle.py
+++ wordle.py
@@ -1,5 +1,5 @@
 # TASK A: Define a variable 'word' that holds the correct word for the wordle game
-word = "apple"
+word = "start"
 
 # TASK B: Define a function 'makeAGuess()' that passes in a users guess as a parameter
 def makeAGuess(guess):
@@ -29,15 +29,19 @@ def makeAGuess(guess):
 print("Let's play wordle! /n Guess the Wordle in 6 tries. Each guess must be a valid 5-letter word. For each guess, a hint will tell you how many letters you've guessed correctly. A G represents a letter in the word and in the correct spot.. A Y represents a letter in the word but in the wrong spot. A - represents a letter not in the word in any spot. \n Guess below! \n")
 
 # TASK I: Build a loop that loops 6 times (representing the number of guesses a user has)
-
-
+for i in range(6):
   # TASK J: Define a variable 'guess'. prompt the user for their 5-letter guess and store it in the variable
-
+  guess = input("make your guess: ")
   # TASK K: Define a variable 'hint' and set the return of makeAGuess(guess) to that variable
-
+  hint = makeAGuess(guess)
   # TASK L: Print hint
-
+  print(hint)
   # TASK M: Check if hint = "GGGGG". If so the user has won. Print a win message and break the loop
-  
+  if hint == "GGGGG":
+    print("you won!")
+    break
+ 
 
 # TASK N: After the loop has finished, meaning the user has run out of guesses, check if hint != "GGGGG". If so, the user has lost. Print a lose message. 
+if hint != "GGGGG":
+  print("try again later")
