9b2ed34a6613a27298edb8bc4b7f9c450df589d2
bread sticks <uubixx.0208@gmail.com>
2024-09-03 18:01:05 +0000

    worked on items and runner

--- items.py
+++ items.py
@@ -1,4 +1,12 @@
 class item:
-    def __init__(self, name, description):
+    def __init__(self, name, description, price):
         self.name = name
         self.description = description
+        self.price = price
+        
+    inventory = []
+
+    def reciveitem(self, item):
+        self.inventory.append(item)
+    
+    
--- ruuner.py
+++ ruuner.py
@@ -1,13 +1,43 @@
 from characters import Character
 from items import item
+
 from wordle import playwordle
 
-inventoy = []
+inventory = []
+money = 0
 
 print("You find yourself in a field. In front of you is a path. There also seems to be someone waiting behind you.")
 choice_1 = input("Continue down the path or turn around? (path or turn)")
 if choice_1 == "path":
-    Print("you go down the path. You eventually see a village")
+    print("you go down the path. You eventually see a village")
+    Choice_a1 = input("you see a stand selling multiple items (look/leave)")
+
+    if Choice_a1 == "look":
+        print("There are a few items up for sale")
+        snowgrass = item("a piece of paper", "a bundle of snow grass.", "$3")
+        paper = item("a piece of paper", "an old looking scrap of paper.", "$1000")
+        print(snowgrass.description, snowgrass.price)
+        print(paper.description, paper.price)
+
+        choice_a2 = input("you currently have: $" + str(money) + " dollars. what would you like to buy? (snowgrass/paper/nothing)")
+        if choice_a2 == "snowgrass":
+            snowgrass.reciveitem(snowgrass)
+            print("you bought the snow grass.")
+            print(inventory)
+            print("you leave the stand.")
+
+        elif choice_a2 == "paper":
+            paper.reciveitem(paper)
+            print("you bought the paper.")
+            print(inventory)
+
+        else:
+            print("you leave the stand.")
+
+    elif Choice_a1 == "leave":
+        print("'you conitinue walking and encounter a wombat in the middle of the path'")
+        
+
 
 elif choice_1 == "turn":
     print("You turn to see what looks like a wandering riddle master.")
@@ -19,4 +49,6 @@ elif choice_1 == "turn":
         
     elif Choice_b1 == "refuse":
         print("'I understand. It sems you just aren't cool enough. Come back when you find your swag......'")
+        print("you decide to go back to the field for now")
+    
 
